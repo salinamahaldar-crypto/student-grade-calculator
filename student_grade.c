514d74d9a51d46e1983ca2c07ea2b74661620d89
salinamahaldar-crypto <salinamahaldar@gmail.com>
2026-09-17 11:41:01 +0530

    Add files via upload

---
+++ student_grade.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    float marks;
+    printf("Enter your marks: ");
+    scanf("%f", &marks);
+    if (marks >= 90)
+        printf("Grade: A");
+    else if (marks >= 75)
+        printf("Grade: B");
+    else if (marks >= 60)
+        printf("Grade: C");
+    else if (marks >= 40)
+        printf("Grade: D");
+    else
+        printf("Grade: F");
+    return 0;
+}
